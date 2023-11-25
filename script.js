5ba2e8757b00d2caf7f0335f7db456f6642ee258
mohammadabubakaratiq <52757937+AbubakarAtiq@users.noreply.github.com>
2023-11-25 12:31:58 +0500

    index-script-style-added
    In this commit, index and script and style files are added for now to showcase.

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript for form submission (You can add more functionality as needed)
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+    e.preventDefault(); // Prevent form submission
+    alert("Form submitted!"); // Display a simple alert (you can replace this with actual form submission code)
+});
